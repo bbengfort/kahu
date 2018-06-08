abe3670111e616b74aad409ee0c98a1f10bce9ab
Benjamin Bengfort <benjamin@bengfort.com>
2018-06-08 12:15:20 -0400

    added links and filtering for replica health

--- replicas/views.py
+++ replicas/views.py
@@ -15,10 +15,14 @@ Controllers and views for the replicas app (HTML/JSON)
 ##########################################################################
 
 from .models import Replica
-from .utils import parse_bool
+from .utils import parse_bool, Health
 
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+ACTIVE = "active"
+HEALTH = "health"
 
 
 ##########################################################################
@@ -32,17 +36,44 @@ class Replicas(LoginRequiredMixin, ListView):
     template_name = "site/replicas.html"
     context_object_name = "replicas"
 
+    def parse_query_string(self):
+        params = {}
+
+        active = self.request.GET.get(ACTIVE, None)
+        if active is not None:
+            params[ACTIVE] = parse_bool(active)
+
+        health = self.request.GET.get(HEALTH, None)
+        if health is not None:
+            params[HEALTH] = Health(health)
+
+        return params
+
     def get_queryset(self):
         queryset = super(Replicas, self).get_queryset()
+        params = self.parse_query_string()
 
-        active = self.request.GET.get('active', None)
-        if active is not None:
-            active = parse_bool(active)
-            queryset = queryset.filter(active=active)
+        if ACTIVE in params:
+            queryset = queryset.filter(active=params[ACTIVE])
+
+        if HEALTH in params:
+            queryset = queryset.active().health(params[HEALTH])
 
         return queryset
+
+    def get_queryset_title(self):
+        params = self.parse_query_string()
+
+        if HEALTH in params:
+            return params[HEALTH].value
+
+        if ACTIVE in params:
+            return "active" if params[ACTIVE] else "inactive"
+
+        return "all"
 
     def get_context_data(self, **kwargs):
         context = super(Replicas, self).get_context_data(**kwargs)
         context['dashboard'] = 'replicas'
+        context['replicas_type'] = self.get_queryset_title()
         return context
